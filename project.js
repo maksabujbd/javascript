f665c344c9b3608c4289f466fbd0dd08536aaf51
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-02 06:59:28 +0600

    determine the number of lines

--- project.js
+++ project.js
@@ -27,5 +27,20 @@ const deposit = () => {
     }
 }
 
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+        if (isNaN(numberOfLines) || numberOfLines < 1 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again");
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
 const depositAmount = deposit();
 console.log("Your deposited amount is: " + depositAmount);
+
+const line = getNumberOfLines();
+console.log("Your selected number of lines: " + line);
