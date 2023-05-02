a73f4cfa888e9f007caf6e56e32826cb118f2a7d
Abdul Kader <mak.sabujbd@gmail.com>
2023-05-02 08:34:24 +0600

    transpose slot machine

--- project.js
+++ project.js
@@ -79,6 +79,7 @@ const spin = () => {
     // console.log(symbols);
     const reels = [[], [], []];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -90,6 +91,30 @@ const spin = () => {
     return reels;
 }
 
+const transpose = (reels) => {
+    const rows = [];
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([])
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol;
+            if (i != row.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+
 let balance = deposit();
 // console.log("Your deposited amount is: " + depositAmount);
 
@@ -99,4 +124,12 @@ const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 
 const reels = spin();
+// console.log(reels);
+
+const rows = transpose(reels);
 console.log(reels);
+console.log(rows);
+
+printRows(rows);
+
+
